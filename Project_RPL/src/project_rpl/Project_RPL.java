6b990c63e23a6f2207ff443822f9019969adee6a
Rigel9802 <luckyrigel9802@gmail.com>
2022-03-19 00:06:45 +0700

    first commit

---
+++ Project_RPL/src/project_rpl/Project_RPL.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package project_rpl;
+
+/**
+ *
+ * @author Lucky
+ */
+public class Project_RPL {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello lucky");
+        System.out.println("Hallo Devanda apakabar apakah kamu baik baik saja");
+        System.out.println("lmao");
+        System.out.println("oyy deva masuk ga");
+    }
+    
+}
